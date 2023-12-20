3735f9bfc3aa8c99b9d08dd7281f6acb183c31c8
hongseokpyo <hongseokpyou@gmail.com>
2023-12-20 10:08:19 +0900

    solution test2

--- 2023/solution_test.cpp
+++ 2023/solution_test.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    std::string line;
+    std::string line;             // std::string 타입으로 line 변수 선언
     ifstream inputFile("in.tex"); // "in.txt" 파일 열기
 
     if (!inputFile.is_open())
@@ -14,9 +14,9 @@ int main()
         return 1;
     }
 
-    while (std::getline(inputFile, line))
+    while (std::getline(inputFile, line)) // std::getline() 함수로 한 줄씩 텍스트를 읽어옴
     {
-        std::cout << "입력 받은 줄: " << line << std::endl;
+        std::cout << "입력 받은 줄: " << line << std::endl; // std::end1은 개행문자
     }
 
     inputFile.close();
