d92543ee0357a1410f021939a69d3bebb5ba3d39
hongseokpyo <hongseokpyou@gmail.com>
2023-12-22 12:45:05 +0900

    Day1 refactoring

--- 2023/Day1_Trebuchet.cpp
+++ 2023/Day1_Trebuchet.cpp
@@ -6,9 +6,8 @@
 using namespace std;
 int main()
 {
-    int ln, rn, i, j, sum = 0, t1, t2, cnt = 0;
+    int i, sum = 0;
     ifstream input_file("input.txt");
-    // vector<string> output;
 
     string patterns[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
@@ -19,20 +18,11 @@ int main()
         size_t pos = 0;
         while (pos < line.length())
         {
-            bool found = false;
-            // while (pos < line.length() && !isdigit(line[pos]))
-            // {
-            //     pos++;
-            // }
-            // if (pos < line.length())
-            //{
             for (i = 0; i < 9; i++)
             {
                 if (line.find(patterns[i], pos) == pos)
                 {
                     result.push_back(i + 1);
-                    // pos += patterns[i].length();
-                    // found = true;
                     pos++;
                     break;
                 }
@@ -49,76 +39,8 @@ int main()
             sum += 10 * result.front() + result.back();
         else if (result.front() == result.back())
             sum += 10 * result[0] + result[0];
-        // }
-        //}
     }
-
-    // for (int num : result)
-    // {
-    //     cout << num << " ";
-    // }
-
     cout << sum << '\n';
 
     return 0;
 }
-
-// string numbers[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
-
-// string line;
-// while (getline(input_file, line))
-// {
-//     for (i = 0; i < 9; i++)
-//     {
-//         size_t found = line.find(patterns[i]);
-//         if (found != string::npos)
-//         {
-//             line.replace(found, patterns[i].length(), numbers[i]);
-//             break;
-//         }
-//         // while (found != string::npos)
-//         // {
-//         //     line.replace(found, patterns[i].length(), numbers[i]);
-//         //     found = line.find(patterns[i], found + 1);
-//         // }
-//     }
-
-//     for (i = 0; i < 9; i++)
-//     {
-//         size_t found = line.rfind(patterns[i]);
-//         if (found != string::npos)
-//         {
-//             line.replace(found, patterns[i].length(), numbers[i]);
-//             break;
-//         }
-//     }
-//     // output.push_back(line);
-//     for (j = 0; line[j] != '\0'; j++)
-//     {
-//         if (48 <= line[j] && line[j] <= 57)
-//         {
-//             ln = line[j] - '0';
-//             break;
-//         }
-//     }
-//     for (j = line.length() - 1; j >= 0; j--)
-//     {
-//         if (48 <= line[j] && line[j] <= 57)
-//         {
-//             rn = line[j] - '0';
-//             break;
-//         }
-//     }
-//     if (ln == rn)
-//     {
-//         t1 = ln * 10 + ln;
-//         sum += t1;
-//     }
-//     else
-//     {
-//         t2 = ln * 10 + rn;
-//         sum += ln * 10 + rn;
-//     }
-//     // cnt++;
-//     // printf("%d %d\n", cnt, t2);
-// }
